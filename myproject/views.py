d5303752e999631a96543120f6f6e4cb8d6ddbe8
SunnyInstitute <128909437+SunnyInstitute@users.noreply.github.com>
2023-03-25 20:32:20 +0530

    Add files via upload

--- myproject/views.py
+++ myproject/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
     return HttpResponse("Hello worlds")
+def about (request):
+    return HttpResponse("about uspage")
